a9496349c3967b14fa0e9c48028d17faede14b27
Caiobjedi <caiobrunoleitedebrito@gmail.com>
2019-10-07 15:35:47 -0300

    criei a tela basica do app com os botoes para pesquisa filtradade acordo com o issue #5

--- eFix/Servico.js
+++ eFix/Servico.js
@@ -1,77 +1,42 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import React, { component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import Firebase from 'firebase';
 
-
-class Servico extends component {
+export default class Servico extends component {
 
     constructor(props){
         
         super(props)
 
-        this.state = {
-
-            name:"",
-            price:"",
-            category:"",
-            ID:"",
-            descrition:"",
-
-        }
+        this.state = { servicos: [] }
     }
+
+   
+render() {
+   /* const emptyList = <View>
+        <Text>Ainda não há cursos cadastrados.</Text>
+    </View>
+*/
+    return (
+        <View style={ styles.container }>
+                       
+                    <Button 
+                        title='Busca Por Categoria' 
+                        onPress={ () => {this.props.navigation.navigate('App')} } />
+                    <Button 
+                        title='Busca Por Preco' 
+                        onPress={ () => {this.props.navigation.navigate('App')} } />
+               
+        </View>
+    );
+}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function Servico(serviceName, servicePrice, serviceCategory, serviceID ){
-
- var name = serviceName;
- var ID = serviceID;
- var price = servicePrice;
- var category = serviceCategory;
-
-    //gets
-    this.getName = function(){
-        return name;
-    };
-
-    this.getID = function(){
-        return ID;
-    };
-
-    this.getPrice = function(){
-        return price;
-    };
-
-    this.getCategory = function(){
-        return category;
-    };
-
-    //sets
-    this.setName = function(value){
-        name = value;
-    };
-
-    this.setID = function(value){
-        ID = value;
-    };
-    
-    this.setCategory = function(value){
-        category = value;
-    };
-
-    this.setPrice = function(value){
-        price = value;    };
-}*/
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
