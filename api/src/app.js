5d4eec95209383f49be24b17b0eb0eda8bc35e72
alvaro-ian <alvaro-ian@hotmail.com>
2019-10-08 23:07:56 -0300

    Adionar endpoint salvar serviço

--- api/src/app.js
+++ api/src/app.js
@@ -15,6 +15,26 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+// Cria servico
+const create = router.post('/', (req, res, next) => {
+    console.log("Requisicao de POST de Servico recebida.");
+    const tipo = req.body.tipo;
+    const nome = req.body.nome;
+    const preco = req.body.preco;
+    const descricao = req.body.descricao;
+
+    const refPath = tipo + "/" + nome;
+    const ref = firebase.database().ref(refPath)
+
+    ref.update({ valor }, function(error) {
+        if (error) {
+            res.send("Dados não poderam ser salvos " + error);
+        } else {
+            res.send("Dados salvos com sucesso " + 200);
+        }
+    });
+});
+
 
 app.use('/', route);
 
