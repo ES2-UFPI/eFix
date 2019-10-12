9180c24b2a2ff7d88713e5575a977cf400e54e72
Edson <edsonbisneto12@gmail.com>
2019-10-12 18:32:25 -0300

    Preço so aceita números e melhoria visual. Issue #2

--- eFix/Cadastro.js
+++ eFix/Cadastro.js
@@ -7,7 +7,8 @@ import {
     TextInput,
     Button, 
     Alert,
-    SafeAreaView
+    SafeAreaView,
+    Picker
 } from 'react-native';
 
 export default class Cadastro extends Component{
@@ -17,7 +18,7 @@ export default class Cadastro extends Component{
   
     this.state = {
       nome:'' ,
-      tipo:'' ,
+      categoria:'eai',
       preco:'' ,
       descricao:''
     };
@@ -29,34 +30,39 @@ export default class Cadastro extends Component{
     return(
     
       <View style={styles.container}>
-        <Text style={styles.text}>Cadastrar um Serviço</Text>
+        <Text style={styles.text}>Cadastrar Serviço</Text>
         
-        <Text>  Nome:</Text>
         <TextInput style={styles.input}  
           placeholder="Nome" 
           underlineColorAndroid="transparent" 
           onChangeText={(nome) => this.setState({nome})}/>
+        
+        <View style={{flexDirection:"row"}}>
+          <Picker
+            selectedValue={this.state.categoria}
+            style={{height: 40, width: 180, backgroundColor:'gainsboro', marginLeft:25, justifyContent:'flex-start', borderRadius:12}}
+            itemStyle={{alignItems:'center', padding:10}}
+            onValueChange={(itemValue) => this.setState({categoria: itemValue})}>
+          <Picker.Item label="Seleciona categoria..." value="" />
+          <Picker.Item label="Jardineiro" value="Jardineiro" />
+          <Picker.Item label="Eletricista" value="Eletricista" />
+          <Picker.Item label="Diarista" value="Diarista" />
+          <Picker.Item label="Encanador" value="Encanador" />
+          </Picker>  
+        
+          <TextInput style={{justifyContent:'flex-end', borderRadius:12, borderWidth:1, borderColor:'gainsboro',marginLeft:18, padding:7, width:100}}  
+            placeholder="Preço" 
+            underlineColorAndroid="transparent"  
+            onChangeText={(preco) => this.setState({preco})}
+            keyboardType={'numeric'}/>
+        </View>  
 
-        <Text>  Tipo de Serviço:</Text>
-        <TextInput style={styles.input}  
-          placeholder="Tipo de Serviço" 
-          underlineColorAndroid="transparent" 
-          onChangeText={(tipo) => this.setState({tipo})}/>
-        
-        <Text>  Preço:</Text>
-        <TextInput style={styles.input}  
-          placeholder="Preço" 
-          underlineColorAndroid="transparent"  
-          onChangeText={(preco) => this.setState({preco})}/>
-        
-        <Text>  Descrição:</Text>
         <TextInput style={styles.D}  
           placeholder="Insira uma descrição para o serviço aqui..." 
           underlineColorAndroid="transparent" 
           onChangeText={(descricao) => this.setState({descricao})}/>
-        
+
         <Button title="Cadastrar" onPress={() => Alert.alert('Simple Button pressed')}/>
-    
       </View>
      
     );
@@ -66,27 +72,30 @@ export default class Cadastro extends Component{
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 30,
     justifyContent: 'center'
   },
   input:{
+    borderRadius:12,
     height:45,
     borderWidth:1,
-    borderColor: 'grey',
+    borderColor: 'gainsboro',
     margin: 20,
     padding: 10
   },
 
   D: {
-    height:60,
+    height:100,
+    borderRadius:12,
     borderWidth:1,
-    borderColor: 'grey',
-    margin: 20,
+    borderColor: 'gainsboro',
+    margin: 15,
     padding: 10
   },
 
   text:{
     textAlign:'center',
+    fontFamily: 'normal',
     fontSize: 20,
     fontWeight: 'bold',
     height:22,
