05cf63b70a9aea84990ae6ead7a891f569b645a4
alvaro-ian <alvaro-ian@hotmail.com>
2019-11-06 15:51:33 -0300

    Iniciar tela de contrato #38

---
+++ src/screens/TelaContrato/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {
+    View
+} from 'react-native';
+
+export default class TelaContrato extends Component {
+    render() {
+        <View>
+            <Text>Contrato de serviço</Text>
+        </View>
+    }
+}
