197b4254873020f30200ac942f88ff094ea28ea1
Isaias <isaigrec@gmail.com>
2019-11-08 06:41:03 -0300

    Adicionar encapsulamento #33

--- src/services/API.js
+++ src/services/API.js
@@ -60,4 +60,76 @@ api.updateCategoryName = async (categoria) => {
 api.deleteCategory = async (categoria) => {
     return await api.delete('/categoria/del', encodeURI(categoria));
 }
+
+// USUARIO
+
+/* 
+EXAMPLE
+
+to create and update a user:
+{
+	"id": "4c5f5b39c126f5d63f7ce1fc7ff93dc2eae5b5a102d832d5287e15",
+	"nome": "Zéca Garganta",
+	"senha": "soufoda01",
+	"email": "meuemail@example.com",
+	"endereco": "Rua das Aguaceiras, 34555, ZP, Teresina, PI"
+}
+
+*/
+api.createUser = async (user) => {
+    return await api.post('/usuario', user);
+}
+
+api.getUser = async (user_id) => {
+    return await api.get('/usuario/' + encodeURI(user_id));
+}
+
+api.updateUser = async (new_values) => {
+    return await api.put('/usuario/', new_values);
+}
+
+api.deleteUser = async (user_id) => {
+    return await api.delete('/usuario/' + encodeURI(user_id));
+}
+
+// PRESTADOR DE SERVICOS
+
+/*
+EXAMPLE
+
+to create a provider:
+{
+	"id_usuario": "óusbduaishbahuydtd3lj44hb13oysdoasgtudo3jg4134o1",
+	"bio": "Sou um eletricista muito louco"
+}
+
+*/
+api.createProvider = async (provider) => {
+    return await api.post('/prestador', provider);
+}
+
+api.getProvidersList = async () => {
+    return await api.get('/prestador');
+}
+
+api.getProvider = async (provider_id) => {
+    return await api.get('/prestador/' + encodeURI(provider_id));
+}
+
+api.updateProvider = async (new_values) => {
+    return await api.put('/prestador', new_values);
+}
+
+api.deleteProvider = async (provider_id) => {
+    return await api.delete('/prestador/' + encodeURI(provider_id));
+}
+
+api.addAvaliationToProvider = async (avaliation) => {
+    return await api.post('/prestador/avaliacao', avaliation);
+}
+
+api.changeProviderDisponibitily = async (provider_id) => {
+    return await api.put('/prestador/status/' + encodeURI(provider_id));
+}
+
 export default api;
