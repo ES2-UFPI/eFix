0bfc8a6b6016d569295c0e216137ab8093cc4536
Edson <edsonbisneto12@gmail.com>
2019-11-08 22:48:44 -0300

    Protótipo de Perfil de Prestadador. Issue #34

---
+++ src/screens/TelaPerfilPrestador.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { 
+    Text, 
+    View,
+    StyleSheet,
+    FlatList,
+    Image,
+    TouchableOpacity
+} from 'react-native';
+
+export default class TelaPerfilPrestador extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            id:'',
+            nomeUsuario: 'Raimundo Nonato',
+            emailUsuario: 'raimundocabecao@yahoo.com.br',
+            enderecoUsuario: "Rua Tchurosbangos Tchudosbagos, 2193",
+            servicosAutorais: ["Nenhum serviço cadastrado."],
+            servicosContratados:'',
+            horariosDisponibilizados:''
+        }
+    }
+
+
+    render(){
+        return (
+            <View>
+                <Text style={styles.top_label}>Perfil de Prestador de Servicos</Text>
+
+                <View style={{alignItems:'center', alignContent: 'center', borderBottomWidth: 1, borderBottomColor:'gainsboro', marginBottom: 10}}>
+                  <Image source={{uri:'http://media.agora.com.vc/thumbs/capas/image_1399.jpg'}} style={{height: 150 , width: 150, marginLeft: 10, marginTop: 20, borderRadius: 150/2}}/>
+                  <View style={{marginTop:20}} >
+                        <Text style={{textAlign:'center', fontSize: 25, color: 'black'}}>{this.state.nomeUsuario}</Text>
+                        <Text style={{textAlign:'center'}}>{this.state.emailUsuario}</Text>
+                        <Text style={{textAlign:'center'}}>{this.state.enderecoUsuario}</Text>
+                        
+                        <View style={{flexDirection:'row'}}>
+                        <TouchableOpacity style={styles.buttons}>
+                          <Text style={{color:'white'}}>Trocar de Perfil</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.buttons}>
+                          <Text style={{color:'white'}}>Cadastrar Servicos</Text>
+                        </TouchableOpacity>
+                      
+
+                        <TouchableOpacity style={styles.buttons}>
+                          <Text style={{color:'white'}}>Editar Serviços</Text>
+                        </TouchableOpacity>
+                        </View>
+
+                  </View> 
+                </View>   
+
+                <View style={{alignContent:'center'}}>
+                      <Text style={{textAlign: 'center'}}>{this.state.servicosAutorais}</Text>
+                </View>
+                
+            </View>    
+        );
+    }
+
+}
+    const styles = StyleSheet.create({
+        top_label:{
+            backgroundColor:"#1e90ff",
+            color: "white",
+            fontWeight: 'bold',
+            fontSize: 24,
+            padding: 20
+        },
+        container:{
+            borderBottomWidth: 1,
+            borderColor: 'grey',
+        },
+        text_name:{
+            color: "black",
+            fontWeight: "bold",
+            fontSize: 15
+        },
+        text_info:{
+            justifyContent: 'center',
+            alignContent: 'center',
+            fontSize: 15
+        },
+        buttons:{
+          margin: 5, 
+          borderWidth: 1, 
+          alignItems:'center', 
+          borderRadius:20, 
+          padding: 10, 
+          height: 40,
+          backgroundColor: 'dodgerblue',
+          borderColor: 'dodgerblue'
+        }
+
+    });
