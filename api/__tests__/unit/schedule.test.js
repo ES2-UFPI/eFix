6c8f5a5725fa11e02ff0dcf3793f5fcb56519ce5
Isaias <isaigrec@gmail.com>
2019-11-21 14:16:04 -0300

    GET horario passando no teste  #40

--- api/__tests__/unit/schedule.test.js
+++ api/__tests__/unit/schedule.test.js
@@ -1,17 +1,19 @@
 const request = require('supertest');
 const app = require('../../src/apps/schedule_app');
+const firebase = require('../../src/firebase_init');
 
 beforeAll(done => {
     done();
 });
   
 afterAll(done => {
+    firebase.database().ref().remove();
     done();
 });
 
 describe('Running Tests...', () =>{
     const json = {
-        id_prestador: "36ec71c245a2f9ad45c6c76aef507eff8b556b6063deef6fe7ceec353a5271a7",
+        id_prestador: "ae4afa056514e6bba1c78e5da9e839f864b5a26d3e8aa5a3f3dd24ee0deb9e87",
         schedule: {
             segunda: [["08:00", "12:00"], ["14:00", "18:00"]],
             terca:   [["08:00", "12:00"], ["14:00", "18:00"]],
@@ -22,6 +24,15 @@ describe('Running Tests...', () =>{
             domingo: []
         }
     }
+
+    const jsonString = {
+        "quarta": [["08:00", "12:00"], ["14:00", "18:00"]],
+        "quinta": [["08:00", "12:00"], ["14:00", "18:00"]], 
+        "sabado": [["08:00", "12:00"]], 
+        "segunda": [["08:00", "12:00"], ["14:00", "18:00"]], 
+        "sexta": [["08:00", "12:00"], ["14:00", "18:00"]], 
+        "terca": [["08:00", "12:00"], ["14:00", "18:00"]]
+    }
     it('create provider schedules', async (done) => {
         const response = await request(app)
         .post('/')
@@ -31,14 +42,14 @@ describe('Running Tests...', () =>{
         done();
     });
 
-    it('get the entire schedule of provider', async () => {
-        const id_prestador = "36ec71c245a2f9ad45c6c76aef507eff8b556b6063deef6fe7ceec353a5271a7";
+    it('get the entire schedule of provider', async (done) => {
+        const id_prestador = "ae4afa056514e6bba1c78e5da9e839f864b5a26d3e8aa5a3f3dd24ee0deb9e87";
 
         const response = await request(app)
         .get('/' + id_prestador)
 
         expect(response.status).toBe(200);
-        expect(response.body).toBe(json);
-
+        expect(response.body).toEqual(jsonString);
+        done();
     });
 });
--- api/src/apps/schedule_app.js
+++ api/src/apps/schedule_app.js
@@ -11,6 +11,7 @@ schedule_app.use(bodyParser.urlencoded({extended: false}));
 
 const router = express.Router();
 
+// cria um novo horario para o prestador
 const create = router.post('/', (req, res, next) => {
     const { id_prestador, schedule } = req.body;
     console.log("CREATE schedule for " + id_prestador);
@@ -18,7 +19,7 @@ const create = router.post('/', (req, res, next) => {
     const horario = schedule;
 
     const refPath = "prestador/" + id_prestador;
-    const ref = firebase.database().ref(refPath)
+    const ref = firebase.database().ref(refPath);
 
     ref.update({ horario }, function(error) {
         if (error) {
@@ -27,8 +28,27 @@ const create = router.post('/', (req, res, next) => {
             res.status(201).send();
         }
     });
+    ref.off("value");
+});
+
+// recupera todo o horario do prestador
+const show = router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log("GET schedule of " + id);
+    
+    const ref = firebase.database().ref("prestador/" + id + '/horario');
+    
+    ref.on("value", function(snapshot){
+        res.json(snapshot.val());
+        ref.off("value");
+    },
+    function(errorObject){
+        console.log("Leitura falhou: " + errorObject.code);
+        res.send(errorObject.code);
+    });
 });
 
 schedule_app.use('/', create);
+schedule_app.use('/', show);
 
 module.exports = schedule_app;
