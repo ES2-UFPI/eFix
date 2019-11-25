f49f079a1b1eff60ac6073dce6c54ce15b39334a
alvaro-ian <alvaro-ian@hotmail.com>
2019-11-25 15:38:12 -0300

    Fazer prototipo do cadastro de horarios #41

--- src/screens/CadastrarHorarios/index.js
+++ src/screens/CadastrarHorarios/index.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import {
     View,
-    Text
+    Text,
 } from 'react-native';
+import Button from '../../components/Button';
+import TextInput from '../../components/SimpleTextInput';
 
 export default class CadastrarHorarios extends Component {
     render() {
         return(
             <View>
-                
+                <Text>Agenda de Trabalho</Text>
+                <Text>Segunda de 8 as 10</Text>
+                <Text>Terça de 14 as 16</Text>
+                <Button>Novo Horário</Button>
+                <TextInput placeholder="Dia"/>
+                <TextInput placeholder="Inicio"/>
+                <TextInput placeholder="Fim"/>
             </View>
         );
     }
